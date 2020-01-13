729dc467d494a7d6b721beda36eedd124df50d4d
jon <jdcorn@live.com>
2020-01-13 12:43:06 -0700

    first commit

---
+++ Alarm31/Views/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm31
+//
+//  Created by Jon Corn on 1/13/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
