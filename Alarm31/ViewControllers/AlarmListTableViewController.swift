2fcd73a46ed7f89f7112acb8aec5cde4d50d80aa
jon <jdcorn@live.com>
2020-01-13 15:57:22 -0700

    added alarm list view controller

--- Alarm31/ViewControllers/AlarmListTableViewController.swift
+++ Alarm31/ViewControllers/AlarmListTableViewController.swift
@@ -21,33 +21,24 @@ class AlarmListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? SwitchTableViewCell else { return UITableViewCell()}
         
-
+        let alarm = AlarmController.shared.alarms[indexPath.row]
+        
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let alarm = AlarmController.shared.alarms[indexPath.row]
+            AlarmController.shared.deleteAlarm(alarm: alarm)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
